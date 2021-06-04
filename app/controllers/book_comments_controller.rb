86131688a259bcf5f42ec78547e7521db9d24284
EC2 Default User <ec2-user@ip-172-31-61-32.ec2.internal>
2021-06-04 04:28:15 +0000

    [UPDATE] コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
+    book_comment.book_id = @book.id
     book_comment.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    @book = Book.find(params[:book_id])
+    # redirect_back(fallback_location: root_path)
   end
   
   private
